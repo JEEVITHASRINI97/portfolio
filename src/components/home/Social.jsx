69581f85ac234e0c7cafcde23048f47f0ad4e8f9
JEEVITHASRINI97 <jeevithasrinivasan97@gmail.com>
2023-08-23 18:06:19 +0530

    portofolio

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Social() {
+  return (
+    <div className="home__social">
+   
+
+      <a
+        href="https://www.linkedin.com/in/jeevitha-srinivasan-38198a113/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/JEEVITHASRINI97"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github"></i>
+      </a>
+    </div>
+  );
+}
+
+export default Social;
---
+++ src/components/work/Data.jsx
@@ -0,0 +1,42 @@
+import Work1 from "../../assets/work1.png";
+import library from '../../assets/Library.png'
+import Work4 from "../../assets/work4.png"
+
+
+export const projectsData = [
+    {
+        id:1,
+        image: Work1,
+        title: 'ICE-FIRE-App',
+        category: "Full Stack Web",
+        techStack: "HTML, CSS, ReactJS, NodeJS, MongoDB, JWT",
+        user:"Email: user@gmail.com", 
+        password:"Password: User@123",
+        demoLink: "	https://rococo-pudding-d98225.netlify.app",
+        sourceCode:"https://github.com/JEEVITHASRINI97/Webcode_ICE-FIRE-API"
+    },
+  
+    {
+        id:2,
+        image: library,
+        title: 'Library Management app',
+        category: "Full Stack Web",
+        techStack: "HTML, CSS, ReactJS, NodeJS, MongoDB, JWT",
+        user:"Username: User", 
+        password:"Password: User",
+        demoLink: "https://https://64e4ea75839a9000721bbd85--sparkling-mochi-914239.netlify.app/",
+        sourceCode:"https://github.com/JEEVITHASRINI97/librarymanagement"
+    },
+
+    {
+        id:3,
+        image: Work4,
+        title: 'CRUD STUDENT-TEACHER',
+        category: "Full Stack App",
+        techStack: "HTML, CSS, ReactJS, NodeJS, MongoDB, Cloudinary",
+        user:"NA", 
+        password:"NA",
+        demoLink: "https://64e4dcb520dd671ffd9faf7a--sage-entremet-44d5c6.netlify.app/",
+        sourceCode:"https://github.com/JEEVITHASRINI97/CRUD-Student-Teacher"
+    },
+]
